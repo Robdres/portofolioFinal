aa702eb9d240a9919d5ff154c63f7790bea35e23
Robdres <robdres123@gmail.com>
2023-05-25 17:25:20 +0000

    added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,46 @@
 <script setup lang="ts">
-import { reactive,ShallowRef,shallowRef } from 'vue';
+import { reactive,watchEffect,shallowRef } from 'vue';
 import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three';
-import { TresCanvas,useRenderLoop } from '@tresjs/core';
+import { extend,TresCanvas,useRenderLoop,useTexture } from '@tresjs/core';
 
-import { OrbitControls } from '@tresjs/cientos';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry'
+import { FontLoader } from 'three/addons/loaders/FontLoader'
 
+import { OrbitControls,Text3D} from '@tresjs/cientos';
+
+extend({ TextGeometry: TextGeometry })
+
+const camera = {
+  position: [2.789917807383607, 1.1520053956903624, 4.066760580672416],
+  fov: 45,
+  rotation: [-0.26033035821639566, 0.3493743923092192, 0.09093149349977715],
+};
+
+const letters = "abcdefghijklmnopqrstuvwxyz, R";
+let interval = null
+
+let func1 = (event) => {  
+  let iteration = 0;
+  clearInterval(interval);
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    iteration += 1 / 3;
+  }, 20);
+}
+const textRef = shallowRef();
 let widthBooks = 5
 let heightBooks = 6
 let lengthBooks = 2
@@ -32,37 +68,17 @@ var tolerance = 0.00001;
 function onClick(ev) {
   console.log(ev)
   if (ev) {
-    if(1-ev.object.position.y<tolerance){
+    if(1-ev.object.position.x<tolerance){
       let aux = setInterval(function() {
-        ev.object.position.y -=0.1;
-        if(ev.object.position.y<tolerance){
+        ev.object.position.x -=0.1;
+        if(ev.object.position.x<tolerance){
           clearInterval(aux)
         }
       }, 10);
     }else {
       let aux = setInterval(function() {
-        ev.object.position.y +=0.1;
-        if(1-ev.object.position.y<tolerance){
-          clearInterval(aux)
-        }
-      }, 10);
-    }
-  }
-}
-function onClick1(ev) {
-  console.log(ev)
-  if (ev) {
-    if(1-ev.object.position.y<tolerance){
-      let aux = setInterval(function() {
-        ev.object.position.y -=0.1;
-        if(ev.object.position.y<tolerance){
-          clearInterval(aux)
-        }
-      }, 10);
-    }else {
-      let aux = setInterval(function() {
-        ev.object.position.y +=0.1;
-        if(1-ev.object.position.y<tolerance){
+        ev.object.position.x +=0.1;
+        if(1-ev.object.position.x<tolerance){
           clearInterval(aux)
         }
       }, 10);
@@ -72,60 +88,107 @@ function onClick1(ev) {
 
 </script>
 
-<template>
-  <TresCanvas v-bind="gl" window-size>
-    <TresPerspectiveCamera :position="[15, 0, 0]"  :look-at="[0, 0, 0]"/>
-    <OrbitControls/>
+<template #fallback>
+<TresCanvas
+    v-bind="gl" 
+    clear-color="#282828"
+    shadows
+    alpha
+    window-size
+    physically-correct-lights
+  >
+    <TresPerspectiveCamera :position="[110, 0, 0]"  :look-at="[0, 0, 0]"/>
+    <OrbitControls />
+    <TresDirectionalLight :position="[100, 10, 10]" :intensity="2" cast-shadow />
+
     <TresGroup 
-      @click="(e)=>{console.log(e)}"
+      :position="[0,0,0]"
+      @click="(e)=>{console.log('hello')}"
     >
-    <TresMesh :position="[0, 0, -3*lengthBooks]" cast-shadow>
+    <TresMesh :position="[0, 0, -3*lengthBooks]" cast-shadow
+      @click="onClick"
+      >
     <TresBoxGeometry :args="[widthBooks, heightBooks, lengthBooks]" />
       <TresMeshToonMaterial color="blue" />
     </TresMesh>
-    <TresMesh :position="[0, 0, -2*lengthBooks]" cast-shadow>
+    <TresMesh :position="[0, 0, -2*lengthBooks]" cast-shadow
+      @click="onClick"
+      >
     <TresBoxGeometry :args="[widthBooks, heightBooks, lengthBooks]" />
       <TresMeshToonMaterial color="blue" />
     </TresMesh>
     <TresMesh ref="boxRef" :position="[0, 0, -lengthBooks]" cast-shadow
-      @pointer-enter="onClick"
-      @pointer-leave="onClick1"
+      @click="onClick"
       >
       <TresBoxGeometry :args="[widthBooks, heightBooks, lengthBooks]"/>
       <TresMeshToonMaterial color="gray" />
     </TresMesh>
-    <TresMesh  :position="[0, 0, 0]" cast-shadow>
+    <TresMesh  :position="[0, 0, 0]" cast-shadow
+      @click="onClick"
+      >
     <TresBoxGeometry :args="[widthBooks, heightBooks, lengthBooks]" />
       <TresMeshToonMaterial color="blue" />
     </TresMesh>
-    <TresMesh :position="[0, 0, lengthBooks]" cast-shadow>
+    <TresMesh :position="[0, 0, lengthBooks]" cast-shadow
+      @click="onClick"
+      >
     <TresBoxGeometry :args="[widthBooks, heightBooks, lengthBooks]" />
       <TresMeshToonMaterial color="blue" />
     </TresMesh>
-    <TresMesh :position="[0, 0, 2*lengthBooks]" cast-shadow>
+    <TresMesh :position="[0, 0, 2*lengthBooks]" cast-shadow
+      @click="onClick"
+      >
     <TresBoxGeometry :args="[widthBooks, heightBooks, lengthBooks]" />
       <TresMeshToonMaterial color="blue" />
     </TresMesh>
-    <TresMesh :position="[0, 0, 3*lengthBooks]" cast-shadow>
+    <TresMesh :position="[0, 0, 3*lengthBooks]" cast-shadow
+      @click="onClick"
+      >
     <TresBoxGeometry :args="[widthBooks, heightBooks, lengthBooks]" />
       <TresMeshNormalMaterial color="blue" />
     </TresMesh>
     </TresGroup>
-    <TresDirectionalLight :position="[0, 15, 15]" :look-at="[0,0,0]" :intensity="1" cast-shadow />
   </TresCanvas>
+  <h1 class="text" data-value="Hi I am," @mouseover="func1">
+    Hi I am,
+  </h1>
+  <h1 class="name" data-value="Roberto Alvarado" @mouseover="func1">
+    Roberto Alvarado 
+  </h1>
+  <span class="text">Gekki</span>
 </template>
 
-
-<style>
+<style >
+@import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500;700&display=swap');
 html,
 body {
+
   margin: 0;
   padding: 0;
   height: 100%;
+  font-family: 'Rajdhani', sans-serif;
   width: 100%;
 }
 #app {
   height: 100%;
   width: 100%;
 }
+.text{
+  padding: 3em;
+  top: 50%;
+  left: 50%;
+  font-family: 'Rajdhani', sans-serif;
+  position: absolute;
+};
+
+.text{
+  transform: translate(-50%,-50%);
+};
+
+.name{
+  color: red;
+
+};
+
 </style>
+
---
+++ src/components/mainText.vue
@@ -0,0 +1,57 @@
+<script>
+const letters = "abcdefghijklmnopqrstuvwxyz, R";
+let interval = null
+
+let hover = (event) => {  
+  let iteration = 0;
+  clearInterval(interval);
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    iteration += 1 / 3;
+  }, 20);
+}
+export default {
+  name: 'MainTextComponent',
+}
+</script>
+
+<template>
+  <div class="main">
+    <h1 class="text" data-value="Hi I am," @mouseover="hover">
+      Hi I am,
+    </h1>
+    <h1 class="text" data-value="Roberto Alvarado" @mouseover="hover">
+      Roberto Alvarado 
+    </h1>
+  </div>
+</template>
+
+<style>
+
+.main{
+  position: absolute;
+  font-family: 'Rajdhani', sans-serif;
+  -webkit-transform: translate(-50%,-50%);
+  padding: 3em;
+  top: 30%;
+  left: 30%;
+  text-align: left;
+  border: red 1px solid;
+};
+.main .text{
+  color:red;
+};
+</style>
